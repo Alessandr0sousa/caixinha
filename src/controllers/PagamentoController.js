2313fde39db89f363ef24028b60ebda8d0c40b54
Alessandr0sousa <alessandro.anali@gmail.com>
2020-11-27 16:23:32 -0300

    atualização na rota  show de pagamento

--- src/controllers/PagamentoController.js
+++ src/controllers/PagamentoController.js
@@ -15,7 +15,7 @@ module.exports = {
         });
     },
     async show(req, res) {
-        con.query('select * from pagamentos where id_pag = ?', [req.params.id], (err, rows) => {
+        con.query('select * from pagamentos where emprestimo = ?', [req.params.id], (err, rows) => {
             try {
                 res.json(rows);
             } catch (error) {
